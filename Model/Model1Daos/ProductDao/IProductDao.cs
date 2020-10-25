07a5e1a0caa23569f7329d339d5391d24c1ae98f
Adrian Garcia Vazquez <adrian.gvazquez@udc.es>
2020-10-25 13:23:20 +0100

    Estructura Daos

---
+++ Model/Model1Daos/ProductDao/IProductDao.cs
@@ -0,0 +1,11 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.ProductDao
+{
+    interface IProductDao : IGenericDao<Product, Int64>
+    {
+        List<Product> FindByProductId(long productId, int startIndex, int count);
+    }
+}
---
+++ Model/Model1Daos/ProductDao/ProductDaoEntityFramework.cs
@@ -0,0 +1,25 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.ProductDao
+{
+    class ProductDaoEntityFramework :
+        GenericDaoEntityFramework<Product, Int64>, IProductDao
+    {
+        public List<Product> FindByProductId(long productId, int startIndex, int count)
+        {
+            DbSet<Product> products = Context.Set<Product>();
+
+            var result =
+                (from b in products
+                 where b.productId == productId
+                 orderby b.productId
+                 select b).Skip(startIndex).Take(count).ToList();
+
+            return result;
+        }
+    }
+}
