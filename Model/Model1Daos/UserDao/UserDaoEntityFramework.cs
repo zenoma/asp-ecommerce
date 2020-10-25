5c2e43b6c450bec98c73da3fe261e7e6b159dec9
Pablo Hermida Mourelle <pablo.hermidam@udc.es>
2020-10-25 20:42:52 +0100

    Crear daos para Movie, Tag y User

---
+++ Model/Model1Daos/UserDao/UserDaoEntityFramework.cs
@@ -0,0 +1,27 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.UserDao
+{
+    class UserDaoEntityFramework :
+        GenericDaoEntityFramework<User, Int64>, IUserDao
+    {
+
+        public User FindByLogin(string login)
+        {
+            DbSet<User> users = Context.Set<User>();
+
+            var result =
+                (from u in users
+                 where u.login.Equals(login)
+                 select u);
+
+            return result.Single();
+        }
+    }
+}
