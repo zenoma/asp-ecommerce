7688e2b723dfddbcbba2c524f2451338943d1bba
Adrian Garcia Vazquez <adrian.gvazquez@udc.es>
2020-12-16 20:53:11 +0100

    Base esqueleto Autenticación Front

---
+++ Web/ECommerce.Master.cs
@@ -0,0 +1,41 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web
+{
+    public partial class ECommerce : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!SessionManager.IsUserAuthenticated(Context))
+            {
+
+                if (lblDash2 != null)
+                    lblDash2.Visible = false;
+                if (lnkUpdate != null)
+                    lnkUpdate.Visible = false;
+                if (lblDash3 != null)
+                    lblDash3.Visible = false;
+                if (lnkLogout != null)
+                    lnkLogout.Visible = false;
+
+            }
+            else
+            {
+                if (lblWelcome != null)
+                    lblWelcome.Text =
+                        GetLocalResourceObject("lblWelcome.Hello.Text").ToString()
+                        + " " + SessionManager.GetUserSession(Context).FirstName;
+                if (lblDash1 != null)
+                    lblDash1.Visible = false;
+                if (lnkAuthenticate != null)
+                    lnkAuthenticate.Visible = false;
+            }
+        }
+    }
+}
---
+++ Web/HTTP/Util/IoC/IoCManagerNinject.cs
@@ -0,0 +1,47 @@
+﻿using Es.Udc.DotNet.ModelUtil.IoC;
+using Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.CreditCardDao;
+using Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.UserDao;
+using Es.Udc.DotNet.PracticaMaD.Model.Services.UserService;
+using Ninject;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.HTTP.Util.IoC
+{
+    internal class IoCManagerNinject : IIoCManager
+    {
+        private static IKernel kernel;
+        private static NinjectSettings settings;
+
+        public void Configure()
+        {
+            settings = new NinjectSettings() { LoadExtensions = true };
+            kernel = new StandardKernel(settings);
+
+            /* UserProfileDao */
+            kernel.Bind<IUserDao>().
+                To<UserDaoEntityFramework>(); 
+            
+            kernel.Bind<ICreditCardDao>().
+                 To<CreditCardDaoEntityFramework>();
+
+            /* UserService */
+            kernel.Bind<IUserService>().
+                To<UserService>();
+
+            /* DbContext */
+            string connectionString =
+                ConfigurationManager.ConnectionStrings["ecommerceEntities"].ConnectionString;
+
+            kernel.Bind<DbContext>().
+                ToSelf().
+                InSingletonScope().
+                WithConstructorArgument("nameOrConnectionString", connectionString);
+        }
+
+        public T Resolve<T>()
+        {
+            return kernel.Get<T>();
+        }
+    }
+}
