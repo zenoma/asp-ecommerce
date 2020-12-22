6945b393f35bdf18d67978b60966db76de2b2d98
Ricardo Sanchez Arias <ricardo.sanchez1@udc.es>
2020-12-22 12:59:24 +0100

    Añadido link en pagina maestra para navegar a search

--- Web/ECommerce.Master.designer.cs
+++ Web/ECommerce.Master.designer.cs
@@ -123,6 +123,24 @@ namespace Es.Udc.DotNet.PracticaMaD.Web
         protected global::System.Web.UI.WebControls.HyperLink lnkLogout;
 
         /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control lnkSearch.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkSearch;
+
+        /// <summary>
         /// Control ContentPlaceHolder_BodyContent.
         /// </summary>
         /// <remarks>
