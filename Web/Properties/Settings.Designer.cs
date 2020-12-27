3cfedd26c2d387410840582999af43baa1aa9a05
Ricardo Sanchez Arias <r.sanchezarias6@gmail.com>
2020-12-27 14:24:34 +0100

    settings paginacion

--- Web/Properties/Settings.Designer.cs
+++ Web/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Es.Udc.DotNet.PracticaMaD.Web.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
         public int ECommerce_defaultCount {
             get {
                 return ((int)(this["ECommerce_defaultCount"]));
