d01df91dc1552bb70e0084f8a04ad9318fd69ec8
Pablo Hermida Mourelle <pablo.hermidam@udc.es>
2021-01-02 15:08:37 +0100

    Add link to remove comment

---
+++ Web/Pages/Comments/RemoveComment.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ECommerce.Master" AutoEventWireup="true" CodeBehind="RemoveComment.aspx.cs" Inherits="Es.Udc.DotNet.PracticaMaD.Web.Pages.Comments.RemoveComment" %>
+<asp:Content ID="Content5" ContentPlaceHolderID="ContentPlaceHolder_BodyContent" runat="server">
+</asp:Content>
---
+++ Web/Pages/Comments/RemoveComment.aspx.cs
@@ -0,0 +1,27 @@
+﻿using Es.Udc.DotNet.ModelUtil.IoC;
+using Es.Udc.DotNet.PracticaMaD.Model.ECommerceServices.CommentService;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Comments
+{
+    public partial class RemoveComment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IIoCManager iocManager = (IIoCManager)HttpContext.Current.Application["managerIoC"];
+            ICommentService commentService = iocManager.Resolve<ICommentService>();
+
+            commentService.RemoveComment(SessionManager.GetUserSession(Context).UserProfileId, 
+                long.Parse(Request.Params.Get("CommentId")));
+
+            Response.Redirect(Response.
+                    ApplyAppPathModifier("~/Pages/Comments/ShowCommentsByUserLogin.aspx"));
+        }
+    }
+}
