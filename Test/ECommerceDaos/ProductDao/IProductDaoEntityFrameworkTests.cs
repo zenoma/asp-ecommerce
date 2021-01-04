4444aa56e860a1614b5202adedd25093c4c3682d
Adrian Garcia Vazquez <adrian.gvazquez@udc.es>
2021-01-04 20:01:51 +0100

    Add test for findByTag in ProductDao

--- Test/ECommerceDaos/ProductDao/IProductDaoEntityFrameworkTests.cs
+++ Test/ECommerceDaos/ProductDao/IProductDaoEntityFrameworkTests.cs
@@ -1,7 +1,11 @@
 ﻿using Es.Udc.DotNet.PracticaMaD.Model;
+using Es.Udc.DotNet.PracticaMaD.Model.ECommerceDaos.RoleDao;
 using Es.Udc.DotNet.PracticaMaD.Model.ECommerceDaos.Util;
 using Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.CategoryDao;
+using Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.CommentDao;
 using Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.ProductDao;
+using Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.TagDao;
+using Es.Udc.DotNet.PracticaMaD.Model.Model1Daos.UserDao;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ninject;
 using System.Collections.Generic;
@@ -15,6 +19,10 @@ namespace Es.Udc.DotNet.PracticaMaD.Test.Model1Daos.ProductDao
         private static IKernel kernel;
         private static IProductDao productDao;
         private static ICategoryDao categoryDao;
+        private static ITagDao tagDao;
+        private static ICommentDao commentDao;
+        private static IRoleDao roleDao;
+        private static IUserDao userDao;
 
         private const long NON_EXISTENT_PRODUCT_ID = -1;
 
@@ -46,6 +54,10 @@ namespace Es.Udc.DotNet.PracticaMaD.Test.Model1Daos.ProductDao
             kernel = TestManager.ConfigureNInjectKernel();
             productDao = kernel.Get<IProductDao>();
             categoryDao = kernel.Get<ICategoryDao>();
+            tagDao = kernel.Get<ITagDao>();
+            commentDao = kernel.Get<ICommentDao>();
+            userDao = kernel.Get<IUserDao>();
+            roleDao = kernel.Get<IRoleDao>();
         }
         //Use ClassCleanup to run code after all tests in a class have run
         [ClassCleanup()]
@@ -204,6 +216,99 @@ namespace Es.Udc.DotNet.PracticaMaD.Test.Model1Daos.ProductDao
 
             Assert.IsTrue(retrievedProducts.Results.Count == 0);
         }
+
+        [TestMethod]
+        public void FindByNameByTagTest()
+        {
+
+            int numberOfProducts = 5;
+
+            List<Product> createdProducts = new List<Product>(numberOfProducts);
+            string name = "Some Name";
+
+            Category category = new Category();
+            category.visualName = "Category";
+            categoryDao.Create(category);
+            Tag newTag = new Tag();
+            newTag.name = "New";
+            tagDao.Create(newTag);
+
+            Tag oldTag = new Tag();
+            oldTag.name = "Old";
+            tagDao.Create(oldTag);
+
+
+            Role role = new Role();
+            role.name = "TEST";
+
+            roleDao.Create(role);
+
+            User user = new User();
+            user.roleId = roleDao.FindByName(role.name).roleId;
+            user.login = "login";
+            user.password = "password";
+            user.name = "name";
+            user.surnames = "surnames";
+            user.email = " email";
+            user.postalAddress = "postalAddress";
+            user.language = "es";
+            user.country = "es";
+
+            userDao.Create(user);
+            user.userId = userDao.FindByLogin(user.login).userId;
+
+            /*Create numberOfProducts products*/
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                Product product = new Product();
+                product.categoryId = category.categoryId;
+                product.name = name;
+                product.productDate = System.DateTime.Now;
+                product.stockUnits = 100;
+                product.unitPrice = 5;
+                product.type = "Tipo";
+                productDao.Create(product);
+                createdProducts.Add(product);
+
+                if (i == 1 || i == 3)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Comment comment = new Comment();
+                        comment.commentDate = System.DateTime.Now;
+                        comment.productId = product.productId;
+                        comment.userId = user.userId;
+                        comment.body = "test comment";
+                        comment.Tag.Add(newTag);
+                        commentDao.Create(comment);
+                    }
+                }
+                else if (i == 2)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Comment comment = new Comment();
+                        comment.commentDate = System.DateTime.Now;
+                        comment.productId = product.productId;
+                        comment.userId = user.userId;
+                        comment.body = "test comment";
+                        comment.Tag.Add(oldTag);
+                        commentDao.Create(comment);
+                    }
+                }
+            }
+
+
+
+            Block<Product> retrievedNewProducts = productDao.FindByTag(newTag.tagId, 1, 10);
+
+            Assert.IsTrue(retrievedNewProducts.Results.Count == 2);
+            
+            Block<Product> retrievedOldProducts = productDao.FindByTag(oldTag.tagId, 1, 10);
+
+            Assert.IsTrue(retrievedOldProducts.Results.Count == 1);
+        }
+
 
     }
 }
